d15a37eacc953e822b9d915d6406bf2b5c722afa
Spencer <sjk7876@rit.edu>
2023-08-09 10:50:12 -0400

    Initial Commit

---
+++ bruteforce.py
@@ -0,0 +1,49 @@
+"""
+This program finds a match for a hashed password via brute force.
+Uses multi-threading to speed up process
+
+@Author Spencer Kurtz
+"""
+
+"""
+password sha1 	- 
+password sha256 - 
+password md5 	- 
+P@ssw0rd sha1	- 
+P@ssword sha256 - 
+P@ssword md5	- 
+"""
+
+"""
+TODO - 
+viewmodel - 
+	sha1 hashing
+	sha256 hashing
+	md5 hashing
+ 
+	import password list
+	random password generation
+ 
+	hash finding
+	
+	gpu threading
+ 
+	potentially cpu multi threading
+
+	option to input a plain-text password + hashing algo
+cli - 
+	extremely basic
+	"dank memes"
+gui - 
+	loading bar
+	select # cores
+	etc
+	"dank memes"
+"""
+
+def main():
+    print()
+
+
+if (__name__ == "__main__"):
+    main()
