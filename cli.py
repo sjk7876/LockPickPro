acc1440fbcbe6be4bcbb49628cac884c28167a3d
Spencer <sjk7876@rit.edu>
2023-08-09 15:19:02 -0400

    updated cli

--- cli.py
+++ cli.py
@@ -1,11 +1,24 @@
 import bruteforce
 
 def main():
-    password, algo = bruteforce.getPasswordFromStdIn()
+    password = "P@ssw0rd"
+    algo = "md5"
+    wordlist = "wordlists/fasttrack.txt"
+    # hash = bruteforce.hashPasswordWithAlgo(password, algo)
     
-    print(bruteforce.hashPassword(password, algo))
+    hash = input("Input hash to use: ")
+
+    
+    # password, algo = bruteforce.getPasswordFromStdIn()
+    
+    # hash = bruteforce.hashPasswordWithAlgo(password, algo)
+
+    # print(bruteforce.hashPasswordWithAlgo(password, algo))
+    
+    # wordlist = "wordlists/" + input("Input the filename of the word list to use: ")
+    
+    print(bruteforce.crackPassword(hash, wordlist))
             
-    # masterHash = input("Input hash to use: ")
 
 
 if (__name__ == "__main__"):
