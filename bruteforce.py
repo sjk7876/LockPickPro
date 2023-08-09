a4335d91eeda22162bf4a850af2d9acbb3d1bda7
Spencer <sjk7876@rit.edu>
2023-08-09 15:18:51 -0400

    bruteforcing with a word list

--- bruteforce.py
+++ bruteforce.py
@@ -17,23 +17,23 @@ P@ssword md5	- 161ebd7d45089b3446ee4e0d86dbcf92
 """
 
 """
-TODO - 
-viewmodel - 
+TODO -
+viewmodel -
 	sha1 hashing
 	sha256 hashing
 	md5 hashing
 
 	import password list
-	
+
 	have list of password wordlists that one can use
 		provide user ability to select wordlist
 	mangle option
-	
+
 	"generating word list"
 	then go to progress bar after
 
 	hash finding
-	
+
 	gpu threading
 
 	potentially bcrypt
@@ -41,46 +41,86 @@ viewmodel -
 
 	option to input a plain-text password + hashing algo
 
-cli - 
+cli -
 	extremely basic
 	"dank memes"
 
-gui - 
+gui -
 	loading bar
 	select # cores
 	etc
 	"dank memes"
- 
+
 SEND TO EXE
 """
 
 
+"""
+Prompts user for a password and hashing algorithm to use
+
+@return	String String, password to hash and algorithm to use
+"""
 def getPasswordFromStdIn():
-    password = input("Enter password to hash: ")
-    algo = input("Enter algorithm to use (MD5, SHA1, SHA256): ").lower()
-    
-    while algo not in ("sha256", "sha1", "md5"):
-        algo = input("Incorrect algorithm entered, please try again: ").lower()
-        
-    return password, algo
+	password = input("Enter password to hash: ")
+	algo = input("Enter algorithm to use (MD5, SHA1, SHA256): ").lower()
+
+	while algo not in ("sha256", "sha1", "md5"):
+		algo = input("Incorrect algorithm entered, please try again: ").lower()
+
+	return password, algo
 
 
-def hashPassword(password, algo):
-    match algo:
-        case "md5":
-            return hashlib.md5(password.encode()).hexdigest()
-        case "sha1":
-            return hashlib.sha1(password.encode()).hexdigest()
-        case "sha256":
-            return hashlib.sha256(password.encode()).hexdigest()
+"""
+Hashes a given password based on the given algorithm
+
+@param password		String, password to be hashed
+@param algo			String, algorithm to use
+@return				String, hex of hash
+"""
+def hashPasswordWithAlgo(password, algo):
+	match algo:
+		case "md5":
+			return hashlib.md5(password.encode()).hexdigest()
+		case "sha1":
+			return hashlib.sha1(password.encode()).hexdigest()
+		case "sha256":
+	  		return hashlib.sha256(password.encode()).hexdigest()
+
+"""
+Given a password, returns a list of hashes
+
+@param password		String, password to hash
+@return				List, list of hash strings in hex
+"""
+def hashPasswordWithoutAlgo(password):
+	return [hashlib.md5(password.encode()).hexdigest(),
+			hashlib.sha1(password.encode()).hexdigest(),
+			hashlib.sha256(password.encode()).hexdigest()]
 
 
-def crackPassword(password):
+"""
+Brute forces a given hash with a wordlist
+If given the mangle flag, it calls a function to 'mangle' the list
 
+@param hash		String, hash to find
+@param file		String, word list to use
+@param mangle	Bool, 
+@return			password the hash is associated with or None
+"""
+def crackPassword(hash, file, mangle):
+	with open(file, "r") as wordListFile:
+		wordList = wordListFile.read().splitlines()
+	
+	# if mangle:
+	# 	wordList = mangleList(wordList)
+
+	for word in wordList:
+		if hash in hashPasswordWithoutAlgo(word):
+			return word
 
 def main():
-    print()
+	print()
 
 
 if (__name__ == "__main__"):
-    main()
+	main()
